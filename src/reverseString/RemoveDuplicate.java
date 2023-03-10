5812046e400e3e40c6d9e90359ab1ecb7ec378f2
Kafle <kaflepk@amazon.com>
2023-03-10 16:15:24 -0500

    two classes added

---
+++ src/reverseString/RemoveDuplicate.java
@@ -0,0 +1,29 @@
+package reverseString;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RemoveDuplicate {
+	
+public static void main(String[] args) {
+	int []arr=new int[] {};
+	List<Integer>list=new ArrayList<Integer>();
+	list.add(2);
+	list.add(3);
+	list.add(2);
+	list.add(3);
+	list.add(4);
+	list.add(5);
+	list.add(6);
+	list.add(7);
+	list.add(8);
+	list.add(9);
+	System.out.println(list.toString());
+	Set<Integer>set=new LinkedHashSet<Integer>(list);
+	System.out.println(set);
+
+	}
+
+}
---
+++ src/reverseString/TwoMaximumValueInArray.java
@@ -0,0 +1,32 @@
+package reverseString;
+
+public class TwoMaximumValueInArray {
+	public static String getTwoLagestNum(int[]arr) {
+		int largestA = arr[0];
+	    int largestB = arr[0];
+	    for(int i = 0; i < arr.length; i++){
+	    	if(arr[i]>largestA) {
+	    		largestA=arr[i];
+
+            }
+            if(arr[i] < largestA){
+                largestB = arr[i];   
+            }
+
+    }
+		return largestA +" "+largestB ;
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		int[]arr=new int[] {5,2,3,10,15,30,20,18,29,40};
+		System.out.println(getTwoLagestNum(arr));
+		System.out.println();
+		
+		
+
+	}
+
+}
