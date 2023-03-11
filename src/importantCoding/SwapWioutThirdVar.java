34286b115314af879a6af4ae22829ad087c9836a
Puru <purukafle123@gmail.com>
2023-03-11 09:43:20 -0500

    swap witout third varable

--- src/importantCoding/SwapWioutThirdVar.java
+++ src/importantCoding/SwapWioutThirdVar.java
@@ -1,9 +1,18 @@
 package importantCoding;
 
 public class SwapWioutThirdVar {
-	public void swapWithoutThirdVariable() {
-		
-		
+	public static void main(String[] args) {
+	        int x = 10;
+	        int y = 20;
+	        System.out.println("Before swap:");
+	        System.out.println("x value: "+x);
+	        System.out.println("y value: "+y);
+	        x = x+y;
+	        y=x-y;
+	        x=x-y;
+	        System.out.println("After swap:");
+	        System.out.println("x value: "+x);
+	        System.out.println("y value: "+y);
+	    }
 	}
-
-}
+	
